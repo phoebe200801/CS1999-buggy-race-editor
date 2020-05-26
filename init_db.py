48ad3c74d452ee59740586a2ac0d06d6960d7532
Anne Example <phoebe200801@phoebes-air-2.home>
2020-05-26 14:13:38 +0100

    power_type added to database

--- init_db.py
+++ init_db.py
@@ -29,7 +29,8 @@ con.execute("""
     qty_wheels            INTEGER DEFAULT 4,
     flag_color            VARCHAR(20),
     flag_color_secondary  VARCHAR(20),
-    flag_pattern          VARCHAR(20)
+    flag_pattern          VARCHAR(20),
+    power_type            VARCHAR(20)
   )
 
 """)
